ebb402f11f221eded77495686c29ff0f5cc3c3b2
ricardodev04 <rapineda@innovacion.gob.sv>
2025-03-10 13:45:27 -0600

    ejercicio 3.6

--- AgendaTelefonica/index.js
+++ AgendaTelefonica/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     { 
       "id": 1,
@@ -52,6 +54,34 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     persons = persons.filter(person => person.id !== id);
     response.status(204).end();
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    const existedName = persons.find(person => person.name === body.name);
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: 'Content missing'
+        })
+    }
+    if(existedName){
+        return response.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body. number,
+        id: generateId()
+    }
+    persons = persons.concat(person);
+    response.json(person);
 })
 const PORT = 3001
 app.listen(PORT, () => {
