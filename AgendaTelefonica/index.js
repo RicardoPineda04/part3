8c8706509b51b459ba5ad87257181f8d3ccd4419
ricardodev04 <rapineda@innovacion.gob.sv>
2025-03-10 13:18:09 -0600

    Ejercicio 3.3

---
+++ AgendaTelefonica/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+    const fecha = new Date();
+    const mensaje = `Phonebook has info for ${persons.length} people`;
+    response.send(`<p>${mensaje}</p><br><p>${fecha.toString()}</p>`);
+    
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+    if(person){
+        response.json(person);
+    }else{
+        return response.status(404).json({
+            error: 'Content Missing'
+        })
+    }
+    
+})
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);    
+})
