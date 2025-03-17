194437d3972e82f0d89d67495137dd3d628809fb
ricardodev04 <rapineda@innovacion.gob.sv>
2025-03-17 09:43:49 -0600

    Ejercicio 3.14

--- AgendaTelefonica/index.js
+++ AgendaTelefonica/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
+morgan.token('body', request => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
@@ -32,7 +36,11 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons);
+        });
 });
 
 app.get('/info', (request, response) => {
@@ -43,16 +51,12 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    if(person){
-        response.json(person);
-    }else{
-        return response.status(404).json({
-            error: 'Content Missing'
-        })
-    }
-    
+    const id = request.params.id
+    Person
+        .findById(id)
+        .then(res => {
+            response.json(res);
+        });    
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -68,26 +72,21 @@ const generateId = () => {
     return maxId + 1
 }
 app.post('/api/persons', (request, response) => {
-    const body = request.body;
-    const existedName = persons.find(person => person.name === body.name);
+    const body = request.body;   
     if(!body.name || !body.number){
         return response.status(400).json({
             error: 'Content missing'
         })
     }
-    if(existedName){
-        return response.status(400).json({
-            error: 'Name must be unique'
-        })
-    }
     const person = {
         name: body.name,
         number: body. number,
-        id: generateId()
     }
-    persons = persons.concat(person);
-    morgan.token('body', request => JSON.stringify(request.body))
-    response.json(person);
+    Person
+        .create(person)
+        .then(res => {
+            response.json(res);
+        })
 })
 const PORT = 3001
 app.listen(PORT, () => {
